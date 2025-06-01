f09c3deb46f6c16451b5ba6b6dd5ab4df2f53d91
jacob.schreiber <jacob.schreiber@clip-c2-63.cbe.vbc.ac.at>
2025-06-01 22:50:23 +0200

    v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bam2bw',
-    version='0.3.0',
+    version='0.3.1',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     scripts=['bam2bw'],
