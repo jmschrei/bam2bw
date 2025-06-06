a6ad0a25838b80feeabe1c7c69563764c7f51e2c
jacob.schreiber <jacob.schreiber@clip-login-0.cbe.vbc.ac.at>
2025-06-06 16:35:43 +0200

    v0.3.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='bam2bw',
-    version='0.3.1',
+    version='0.3.2',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     scripts=['bam2bw'],
     url='https://github.com/jmschrei/bam2bw',
     license='LICENSE.txt',
-    description='A command-line tool for converting SAM/BAM files into stranded bp resolution BigWig files. ',
+    description='A command-line tool for converting SAM/BAM/fragment.tsv files into un/stranded bp resolution BigWig files. ',
     install_requires=[
         "numpy",
         "pysam",
